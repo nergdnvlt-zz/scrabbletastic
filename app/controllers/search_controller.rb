c77654d282bf81be945cbebae519dc02898d5690
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-30 09:31:46 -0600

    built out view to handle objects the way I want, then using that to drive the service and objects I think I want.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,38 @@
 class SearchController < ApplicationController
   def index
+    word = params['q']
+    @search = SearchService.new(word).message
+  end
+end
+
+class SearchService
+  def initialize(word)
+    @word = word
+  end
+
+  def conn
+    Faraday.new(ENV['dict_base_url'])
+  end
+
+  def response
+    conn.get do |req|
+      req.url "inflections/en/#{@word}"
+      req.headers['app_id'] = ENV['app_id']
+      req.headers['app_key'] = ENV['app_key']
+    end
+  end
+
+  def parse
+    JSON.parse(response.body)
+  end
+
+  def message
+    Word.new(parse).message
+  end
+end
+
+class Word
+  def initialize(attrs)
+
   end
 end
--- spec/features/user_validates_word_spec.rb
+++ spec/features/user_validates_word_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 feature 'User fills in field with word' do
   scenario 'it validates that word' do
-    # Background:
-    # * This story should use the Oxford Dictionaries API https://developer.oxforddictionaries.com/documentation
-    # * Use endpoint "GET /inflections/{source_lang}/{word_id}" under the "Lemmatron" heading
+    VCR.use_cassette('features/user_can_validate_word') do
+      # Background:
+      # * This story should use the Oxford Dictionaries API https://developer.oxforddictionaries.com/documentation
+      # * Use endpoint "GET /inflections/{source_lang}/{word_id}" under the "Lemmatron" heading
 
-    # When I visit "/"
-    visit '/'
-    # And I fill in a text box with "foxes"
-    fill_in 'q', with: 'fox'
-    # And I click "Validate Word"
-    click_on 'Validate Word'
-    # Then I should see a message that says "'foxes' is a valid word and its root form is 'fox'."
-    expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
+      # When I visit "/"
+      visit '/'
+      # And I fill in a text box with "foxes"
+      fill_in 'q', with: 'foxes'
+      # And I click "Validate Word"
+      click_on 'Validate Word'
+      # Then I should see a message that says "'foxes' is a valid word and its root form is 'fox'."
+      expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
+    end
   end
-  # 
+  #
   # scenario 'it gives feedback that a word is not valid' do
   #   # When I visit "/"
   #   visit '/'
