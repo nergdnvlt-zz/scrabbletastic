3c2a55973d305c40fbe393cfa58e818bd35d8f89
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-30 10:16:22 -0600

    Need to figure out how to serialize id as game_id.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get '/search', to: 'search#index', as: 'search'
+
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:show]
+    end
+  end
 end
--- spec/requests/api/v1/games/user_sends_games_get_request_spec.rb
+++ spec/requests/api/v1/games/user_sends_games_get_request_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe 'it can get a single game' do
+  before(:each) do
+    josh = User.create(id: 1, name: "Josh")
+    sal = User.create(id: 2, name: "Sal")
+
+    game = Game.create(player_1: josh, player_2: sal)
+
+    josh.plays.create(game: game, word: "sal", score: 3)
+    josh.plays.create(game: game, word: "zoo", score: 12)
+    sal.plays.create(game: game, word: "josh", score: 14)
+    sal.plays.create(game: game, word: "no", score: 2)
+  end
   it 'returns a json response' do
     # Background: This story assumes the base data from running `rake db:seed`
     # When I send a GET request to "/api/v1/games/1" I receive a JSON response as follows:
@@ -20,6 +31,7 @@ describe 'it can get a single game' do
     #   ]
     # }
     expect(response).to be_successful
+    binding.pry
     game = JSON.parse(response.body)
 
     expect(game['game_id']).to eq(1)
