c83196bb7d69d240e4444d4d8d330d2b0f1e2537
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-30 11:26:13 -0600

    Refactoring in small steps.

--- app/controllers/api/v1/games/plays_controller.rb
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::Games::PlaysController < ApplicationController
+  before_action :set_game, :set_player
+
   def create
-    game = Game.find(params[:game_id])
-    player = User.find(params['user_id'])
-    player.plays.create(game: game, word: params['word'])
-    render json: game.as_json, status: 201
+    set_player.plays.create(game: set_game, word: params['word'])
+    render json: set_game.as_json, status: 201
+  end
+
+  private
+
+  def set_game
+    Game.find(params[:game_id])
+  end
+
+  def set_player
+    User.find(params['user_id'])
   end
 end
