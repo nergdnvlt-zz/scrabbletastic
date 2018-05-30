3c3d7eb3f42466fa02d53a75667da760b27649a1
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-30 11:21:30 -0600

    Breaking service into seperate file.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,30 +4,3 @@ class SearchController < ApplicationController
     @search ||= SearchService.new(word)
   end
 end
-
-class SearchService
-  def initialize(word)
-    @word = word
-  end
-
-  def conn
-    Faraday.new(ENV['dict_base_url'])
-  end
-
-  def response
-    conn.get do |req|
-      req.url "inflections/en/#{@word}"
-      req.headers['app_id'] = ENV['app_id']
-      req.headers['app_key'] = ENV['app_key']
-    end
-  end
-
-  def parse
-    return nil if response.body.include?('404')
-    JSON.parse(response.body)
-  end
-
-  def message
-    Word.new(parse, @word).message
-  end
-end
---
+++ app/services/search_service.rb
@@ -0,0 +1,28 @@
+class SearchService
+  def initialize(word)
+    @word = word
+  end
+
+  def message
+    Word.new(parse, @word).message
+  end
+
+  private
+
+  def conn
+    Faraday.new(ENV['dict_base_url'])
+  end
+
+  def response
+    conn.get do |req|
+      req.url "inflections/en/#{@word}"
+      req.headers['app_id'] = ENV['app_id']
+      req.headers['app_key'] = ENV['app_key']
+    end
+  end
+
+  def parse
+    return nil if response.body.include?('404')
+    JSON.parse(response.body)
+  end
+end
