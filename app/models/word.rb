1c7c842e01c490448fc2885c3accd7091a95d8ea
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-02 12:16:00 -0600

    Refactoring the word object to initialize itself.

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,18 +1,34 @@
+# class Word
+#   def initialize(attrs, original_word)
+#     @attrs = attrs
+#     @original_word = original_word
+#   end
+#
+#   def self.message(attrs, original_word)
+#     new(attrs, original_word)
+#     binding.pry
+#     if attrs == nil
+#       "'#{original_word}' is not a valid word."
+#     else
+#       "'#{original_word}' is a valid word and its root form is '#{root_word(attrs)}'."
+#     end
+#   end
+#
+#   private
+#
+#   def root_word(attrs)
+#     attrs['results'][0]['lexicalEntries'][0]['inflectionOf'][0]['id']
+#   end
+# end
+
+
 class Word
-  def initialize(attrs, original_word)
-    @attrs = attrs
-    @original_word = original_word
-  end
-
-  def root_word
-    @attrs['results'][0]['lexicalEntries'][0]['inflectionOf'][0]['id']
-  end
-
-  def message
-    if @attrs == nil
-      "'#{@original_word}' is not a valid word."
+  def self.message(attrs, original_word)
+    new
+    if attrs == nil
+      "'#{original_word}' is not a valid word."
     else
-    "'#{@original_word}' is a valid word and its root form is '#{root_word}'."
+      "'#{original_word}' is a valid word and its root form is '#{attrs['results'][0]['lexicalEntries'][0]['inflectionOf'][0]['id']}'."
     end
   end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -14,9 +14,7 @@ describe Word do
                                             "text"=>"foxes"},
                         {"grammaticalFeatures"=>[{"text"=>"Plural", "type"=>"Number"}], "inflectionOf"=>[{"id"=>"fox", "text"=>"fox"}], "language"=>"en", "lexicalCategory"=>"Noun", "text"=>"foxes"}],
     "word"=>"foxes"}]}
-    word = Word.new(attrs, 'foxes')
-    message = word.message
-    expect(word).to be_a Word
+    message = Word.message(attrs, 'foxes')
     expect(message).to eq("'foxes' is a valid word and its root form is 'fox'.")
   end
 
@@ -33,17 +31,17 @@ describe Word do
                                             "text"=>"foxes"},
                         {"grammaticalFeatures"=>[{"text"=>"Plural", "type"=>"Number"}], "inflectionOf"=>[{"id"=>"fox", "text"=>"fox"}], "language"=>"en", "lexicalCategory"=>"Noun", "text"=>"foxes"}],
     "word"=>"foxes"}]}
-    word = Word.new(attrs, 'lame')
-    message = word.message
-    expect(word).to be_a Word
+    message = Word.message(attrs, 'lame')
+    # message = word.message
+    # expect(word).to be_a Word
     expect(message).to eq("'lame' is a valid word and its root form is 'lame'.")
   end
 
   it 'initiates with nil attributes' do
     attrs = nil
-    word = Word.new(attrs, 'foxez')
-    message = word.message
-    expect(word).to be_a Word
+    message = Word.message(attrs, 'foxez')
+    # message = word.message
+    # expect(word).to be_a Word
     expect(message).to eq("'foxez' is not a valid word.")
   end
 end
